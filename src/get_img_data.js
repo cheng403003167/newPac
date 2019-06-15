8b2753d56e0947c24b3fd7b25a6b04d40f7b6358
unknown <403003167@qq.com>
2019-06-15 18:11:22 +0800

    git add . && git commit -m 'a'

--- src/get_img_data.js
+++ src/get_img_data.js
@@ -1,11 +1,50 @@
-class getImgData {
-  constructor(){
+const EventEmitter = require('events');
+class getImgData extends EventEmitter {
+  constructor({startPage=0,endPage=0}){
+    super();
     this.puppeteer = require('puppeteer');
+    this.temp_data = [];
+    this.startPage = 0;
+    this.currentPage = 0;
+    this.endPage = 0;
+    if(startPage){
+      this.startPage = startPage
+    }
+    if(endPage){
+      this.endPage = endPage
+    }
   }
   async init(){
     this.browser = await this.puppeteer.launch({headless:true});
     this.page = await this.browser.newPage();
-    await this.getData();
+    await this.page.setRequestInterception(true);
+    this.page.on('request', interceptedRequest => {
+      if (interceptedRequest.url().endsWith('.png') || interceptedRequest.url().endsWith('.jpg') || interceptedRequest.url().endsWith('.gif')){
+        interceptedRequest.abort();
+      }else if(interceptedRequest.url().endsWith('.js') && interceptedRequest.url().indexOf('jquery.min.js') < 0 ){
+        interceptedRequest.abort();
+      }else{
+        interceptedRequest.continue();
+      }
+    });
+    await this.getPageNumber();
+  }
+  async getPageNumber(){
+    this.page.on('domcontentloaded',async ()=>{
+      const tempPage = await this.page.evaluate(()=>{
+        return parseInt($(".cp-pagenavi:eq(0) a:eq(0)").text()) + 1;
+      });
+      if(this.startPage != 0){
+        if(this.startPage > tempPage){
+          console.log('给定的开始页码太大');
+          this.closeB();
+        }
+      }else{
+        this.startPage = this.currentPage = tempPage;
+      }
+      await this.getData();
+    })
+    await this.page.goto('http://jandan.net/pic/');
   }
   async getData(){
     this.page.on('domcontentloaded',async ()=>{
@@ -14,16 +53,38 @@ class getImgData {
         var linkArr = [];
         for(var t = 0;t<lisL;t++){
           if($(".commentlist li").eq(t).attr('id') != undefined){
-            linkArr.push('http:'+$(".commentlist li").eq(t).find('.text').find('p').find('a').attr('href'));
+            if($(".commentlist li").eq(t).find('.text').find('p').find('a').attr('href') != 'javascript:;'){
+              linkArr.push('http:'+$(".commentlist li").eq(t).find('.text').find('p').find('a').attr('href'));
+            }
           }
-        }
+        }                    
         return linkArr;
       });
-      console.log(result);
-    })
-    await this.page.goto('http://jandan.net/pic/page-106#comments');
-    // await browser.close();
+      this.temp_data = result;
+      if(this.currentPage > this.endPage){
+        this.currentPage--;
+        this.emit('pageDateCom');
+      }else{
+        this.emit('allDateCom');
+      }
+    });
+    if(this.startPage > 0){
+      await this.page.goto(`http://jandan.net/pic/page-${this.currentPage}#comments`);
+    }
+  }
+  async closeB(){
+    await this.browser.disconnect();
+    await this.browser.close();
   }
 }
-let sets = new getImgData();
-sets.init()
+(async ()=>{
+  let sets = await new getImgData({startPage:0,endPage:107});
+  sets.on('pageDateCom',async ()=>{
+    sets.getData();
+  });
+  sets.on('allDateCom',async ()=>{
+    console.log(sets.temp_data);
+    sets.closeB();
+  })
+  await sets.init()
+})()
